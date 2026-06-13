105a0b2d0d259c0306ee21881b496aa64ff41a82
Vladimir <sovliv@yandex.ru>
2026-06-13 16:54:04 +0300

    added chapter 6

---
+++ solutions/06_Pointers_and_errors/Stringer.go
@@ -0,0 +1,5 @@
+package Pointers_and_errors
+
+type Stringer interface {
+	String() string
+}
---
+++ solutions/06_Pointers_and_errors/Wallet.go
@@ -0,0 +1,33 @@
+package Pointers_and_errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
